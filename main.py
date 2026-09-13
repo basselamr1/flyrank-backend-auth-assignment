b18f8e42d8d103b2f88f199ca7e04fccc4b2cd75
unknown <basselamr1@gmail.com>
2026-09-13 04:10:26 +0300

    Stage 1: signup and login routes working

--- main.py
+++ main.py
@@ -1,21 +1,75 @@
 import os 
 from dotenv import load_dotenv
 from supabase import create_client, Client
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 load_dotenv(override = True)
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 PORT = int(os.getenv("PORT"))
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise RuntimeError("SUPABASE_URL and SUPABASE_KEY must be set")
-    
+
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
 
+class AuthRequest(BaseModel):
+    email: str | None = None
+    password: str | None = None
+
 @app.get('/')
 async def root():
     return {"message": "Server running and connected to Supabase."}
+
+@app.post('/auth/signup', status_code=status.HTTP_201_CREATED)
+async def signup(request: AuthRequest):
+    if request.email is None or request.password is None: 
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail= "Missing email or password."
+        )
+    try:
+        response = supabase.auth.sign_up(
+            {
+                "email": request.email,
+                "password": request.password
+            }
+        )
+        return response.user
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+
+@app.post('/auth/login', status_code= status.HTTP_200_OK)
+async def login(request: AuthRequest):
+    if request.email is None or request.password is None:
+        raise HTTPException(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            detail = "Email or password is missing."
+        )
+    try:
+        response = supabase.auth.sign_in_with_password(
+            {
+                "email": request.email,
+                "password": request.password
+            }
+        )
+        return {
+            "access_token": response.session.access_token,
+            "refresh_token": response.session.refresh_token
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={"error": "Invalid login credentials"}
+        )
+
+    
+
